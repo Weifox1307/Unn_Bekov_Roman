13c75f1b506950df947f55e66328e6697701a3fd
Roman Bekov <bekovroman0@gmail.com>
2026-04-23 23:16:38 +0300

    Delete all_labs_технологии баз данных directory

--- all_labs_технологии баз данных/Лабораторная 1_Беков Роман_3824Б1ПР4.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    TO_CHAR(q_date, 'MM.YYYY') AS "Месяц Год", 
-    SUM(amount) AS "Сумма"
-FROM quotation_data
-WHERE q_date >= '2025-01-01' AND q_date <= '2025-02-28' 
-GROUP BY 1 ORDER BY 1;
